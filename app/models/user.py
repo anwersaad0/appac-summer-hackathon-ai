e67cd688e182e8798a6231c5339f9882ee6468ce
anwersaad0 <anwer.saad0@gmail.com>
2024-07-24 11:32:04 -0700

    added preferred language column to user table

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    pref_lang = db.Column(db.String, nullable=False)
+
     @property
     def password(self):
         return self.hashed_password
@@ -29,5 +31,6 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'prefLang': self.pref_lang
         }
