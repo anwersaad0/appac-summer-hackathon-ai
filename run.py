a5d53aa43ca23fe73efc243cdb69fdf5d026943a
TStutsman <teagandstutsman@gmail.com>
2024-07-24 19:13:10 -0400

    added run.py file for websockets

---
+++ run.py
@@ -0,0 +1,17 @@
+from app import app
+from app.websocket import socketio
+import os
+environment = os.getenv('FLASK_ENV')
+
+cors_list = [] if environment == 'production' else [
+    'http://localhost:5173', 
+    'http://127.0.0.1:5173', 
+    'http://localhost:8000', 
+    'http://127.0.0.1:8000'
+]
+    
+socketio.init_app(app, manage_session=False, cors_allowed_origins=cors_list)
+
+# Starts the app with socketio support
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=8000)
