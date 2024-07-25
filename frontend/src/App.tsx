7b621007aa48a6b8c680c54da1759f058d557f00
leonelcolina <tang.leonel@gmail.com>
2024-07-25 14:06:02 -0400

    frontend boilerplate created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Login from "./pages/Login"
+
+function App() {
+
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
+  )
+}
+
+export default App
