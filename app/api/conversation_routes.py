a8fcb8873dab7974437c69cf624057a10bf6bd7a
anwersaad0 <anwer.saad0@gmail.com>
2024-07-26 20:28:36 -0700

    started on conversation routes

--- app/api/conversation_routes.py
+++ app/api/conversation_routes.py
@@ -1,3 +1,18 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User, Message, Conversation, db
+
+conversation_routes = Blueprint('conversations', __name__)
+
+@conversation_routes.route('/')
+@login_required
+def get_conversations():
+    convos = Conversation.query.filter(current_user.id in Conversation.participants)
+    return {'conversations': [conversation.to_dict() for conversation in convos]}
+
+@conversation_routes.route('/new', methods=["POST"])
+@login_required
+def start_conversation():
+    #form here
+    
+    return {'errors'}
