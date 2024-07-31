0bd4738bb4d8cab3adce110904a976be4c3f170c
anwersaad0 <anwer.saad0@gmail.com>
2024-07-31 13:10:28 -0700

    added regex checker for email field in signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
+import re
 
 
 def user_exists(form, field):
@@ -19,9 +20,18 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def valid_email(form, field):
+# Checking if username is already in use
+    email = field.data
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+    if (re.fullmatch(regex, email)):
+        return
+    else:
+        raise ValidationError('Email is not valid.')
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists, valid_email])
     password = StringField('password', validators=[DataRequired()])
